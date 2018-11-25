16ea839130f084cbc0f8f12083e04b3d7b954e09
Elohim Tenorio <elohim@Elohims-MacBook-Pro.local>
2018-11-25 07:02:52 -0300

    Emit nue Message on create message

--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ var io = socketIO(server);
 io.on('connection',(socket)=>{
   console.log('New User connected');
 
+  socket.on('createMessage',(message)=>{
+    console.log('createMessage',message);
+    io.emit('newMessage',{
+      from:message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
   socket.on('disconnect',()=>{
     console.log('User was disconnected');
   })
