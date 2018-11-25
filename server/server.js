3f59bd71d414f006332c8d31be59f3d310d6839d
Elohim Tenorio <elohimtenorio@gmail.com>
2018-11-25 07:29:19 -0300

    Greate new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,6 +13,16 @@ var io = socketIO(server);
 
 io.on('connection',(socket)=>{
   console.log('New User connected');
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to the chat app'
+  })
+
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New user joined',
+    createdAt:new Date().getTime()
+  });
 
   socket.on('createMessage',(message)=>{
     console.log('createMessage',message);
@@ -21,6 +31,11 @@ io.on('connection',(socket)=>{
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage',{
+    //   from:message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect',()=>{
